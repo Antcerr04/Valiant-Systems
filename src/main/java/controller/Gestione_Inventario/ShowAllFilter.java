5854f5ac7a4fb9fb7cbb77a019dc5f03aa854b9f
Antcerr04 <antoniocerrone132@gmail.com>
2026-01-09 19:42:31 +0100

    Added logout function, search product function and added functionality to show all products with Cliente's view and Manager's view

---
+++ src/main/java/controller/Gestione_Inventario/ShowAllFilter.java
@@ -0,0 +1,55 @@
+package controller.Gestione_Inventario;
+
+import com.google.gson.Gson;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.gestioneinventario.ProdottoDAO;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+//This Servlet is used to add cpu and gpu in select element within showAll.jsp
+@WebServlet(name = "ShowAllFilter", urlPatterns = {"/getCPU", "/getGPU"})
+public class ShowAllFilter extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            if (req.getServletPath().equals("/getCPU")) {
+                ProdottoDAO service = new ProdottoDAO();
+                List<String> cpuList = service.doGetCPUlist();
+                PrintWriter writer = resp.getWriter();
+                resp.setContentType("application/json");
+                resp.setCharacterEncoding("UTF-8");
+                Gson gson = new Gson();
+                writer.print(gson.toJson(cpuList));
+                writer.flush();
+            }
+            if (req.getServletPath().equals("/getGPU")) {
+                if (req.getParameter("cpu") != null && !req.getParameter("cpu").isEmpty()) {
+                    ProdottoDAO service = new ProdottoDAO();
+                    List<String> gpuList = service.doGetGPUlist(req.getParameter("cpu"));
+                    PrintWriter writer = resp.getWriter();
+                    resp.setContentType("application/json");
+                    resp.setCharacterEncoding("UTF-8");
+                    Gson gson = new Gson();
+                    writer.print(gson.toJson(gpuList));
+                    writer.flush();
+                }
+            }
+        } catch (Exception e) {
+            req.setAttribute("exception", e);
+            RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/results/error.jsp");
+            rd.forward(req, resp);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
