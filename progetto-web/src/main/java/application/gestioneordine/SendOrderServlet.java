9e059599fecc3aad7ecb85a5b29423498b24d545
ccunisa <c.corniola1@studenti.unisa.it>
2026-02-07 19:42:50 +0100

    Added testing for SendOrderServlet

--- progetto-web/src/main/java/application/gestioneordine/SendOrderServlet.java
+++ progetto-web/src/main/java/application/gestioneordine/SendOrderServlet.java
@@ -12,22 +12,46 @@ import java.io.IOException;
 
 @WebServlet(name = "SendOrderServlet",urlPatterns = {"/manage-orders/send"})
 public class SendOrderServlet extends HttpServlet {
+    private FacadeDAO dao = new FacadeDAO();
+
+    public void setFacadeDAO(FacadeDAO dao) {
+        this.dao = dao;
+    }
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
         Utente utente = (Utente) session.getAttribute("utente");
         if(utente != null) { //check if user is present in the session (logged in)
-            if(utente.getRuolo()=="manager"){
-                FacadeDAO dao = new FacadeDAO();
-                int orderID = Integer.parseInt(request.getParameter("id"));
-                if(dao.sendOrder(orderID)){
-                    Cookie cookie = new Cookie("notification", "Ordine-con-id-" + orderID + "-inviato-con-successo!");
-                    cookie.setMaxAge(1);
-                    cookie.setSecure(true);
-                    response.addCookie(cookie);
-                    response.sendRedirect(request.getContextPath()+"\\manage-orders");
+            if(utente.getRuolo().equals("manager")){
+                String id = request.getParameter("id");
+                if(id != null) {
+                    if(id.matches("^[1-9][0-9]*$")){
+                        int orderID = Integer.parseInt(id);
+                        try{
+                            if(dao.sendOrder(orderID)){
+                                Cookie cookie = new Cookie("notification", "Ordine-con-id-" + orderID + "-inviato-con-successo!");
+                                cookie.setMaxAge(1);
+                                cookie.setSecure(true);
+                                response.addCookie(cookie);
+                                response.sendRedirect(request.getContextPath()+"\\manage-orders");
+                            }else{
+                                request.setAttribute("errorMSG", "Ordine con id " + orderID + " non trovato o già inviato!");
+                                RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/results/error.jsp");
+                                dispatcher.forward(request, response);
+                            }
+                        } catch (RuntimeException e) {
+                            request.setAttribute("exception", e);
+                            RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/results/error.jsp");
+                            dispatcher.forward(request, response);
+                        }
+
+                    }else{
+                        request.setAttribute("errorMSG", "Il campo id non rispetta il formato!");
+                        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/results/error.jsp");
+                        dispatcher.forward(request, response);
+                    }
                 }else{
-                    request.setAttribute("errorMSG", "Ordine con id " + orderID + " non trovato o già inviato!");
+                    request.setAttribute("errorMSG", "Il campo id deve essere specificato!");
                     RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/results/error.jsp");
                     dispatcher.forward(request, response);
                 }
---
+++ progetto-web/src/test/java/application/gestioneordine/SendOrderServletTest.java
@@ -0,0 +1,112 @@
+package application.gestioneordine;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import storage.FacadeDAO;
+import storage.gestioneutente.Utente;
+
+import java.io.IOException;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+public class SendOrderServletTest {
+    private SendOrderServlet servlet;
+    private HttpServletRequest request;
+    private HttpServletResponse response;
+    private HttpSession session;
+    private RequestDispatcher dispatcher;
+    private FacadeDAO daoMock;
+
+    @BeforeEach
+    void setUp() {
+        servlet = new SendOrderServlet();
+        request = mock(HttpServletRequest.class);
+        response = mock(HttpServletResponse.class);
+        session = mock(HttpSession.class);
+        dispatcher = mock(RequestDispatcher.class);
+        daoMock = mock(FacadeDAO.class);
+        servlet.setFacadeDAO(daoMock);
+
+        when(request.getSession()).thenReturn(session);
+        when(request.getRequestDispatcher(anyString())).thenReturn(dispatcher);
+
+    }
+    @Test
+    public void TC_2_4_1testUtenteNULL() throws ServletException, IOException {
+        when(session.getAttribute("utente")).thenReturn(null);
+
+        servlet.doGet(request, response);
+        verify(response).sendRedirect(eq("null\\login.jsp"));
+        verify(dispatcher, never()).forward(request, response);
+    }
+    @Test
+    public void TC_2_4_2testUtenteClient() throws ServletException, IOException {
+        Utente utente = mock(Utente.class);
+        when(session.getAttribute("utente")).thenReturn(utente);
+        when(utente.getRuolo()).thenReturn("cliente");
+        servlet.doGet(request, response);
+
+        verify(dispatcher).forward(request, response);
+        verify(request).setAttribute(eq("errorMSG"), eq("Operazione negata! Non si hanno i permessi corretti per la risorsa richiesta."));
+    }
+
+    @Test
+    public void TC_2_4_3NoID() throws ServletException, IOException {
+        Utente utente = mock(Utente.class);
+        when(session.getAttribute("utente")).thenReturn(utente);
+        when(utente.getRuolo()).thenReturn("manager");
+        when(request.getParameter("id")).thenReturn(null);
+
+        servlet.doGet(request, response);
+        verify(dispatcher).forward(request, response);
+        verify(request).setAttribute(eq("errorMSG"), eq("Il campo id deve essere specificato!"));
+
+    }
+
+    @Test
+    public void TC_2_4_4IDFormatError() throws ServletException, IOException {
+        Utente utente = mock(Utente.class);
+        when(session.getAttribute("utente")).thenReturn(utente);
+        when(utente.getRuolo()).thenReturn("manager");
+        when(request.getParameter("id")).thenReturn("01");
+
+        servlet.doGet(request, response);
+        verify(dispatcher).forward(request, response);
+        verify(request).setAttribute(eq("errorMSG"), eq("Il campo id non rispetta il formato!"));
+
+    }
+
+
+
+    @Test
+    public void TC_2_4_5ErroreDB() throws ServletException, IOException {
+        Utente utente = mock(Utente.class);
+        when(session.getAttribute("utente")).thenReturn(utente);
+        when(utente.getRuolo()).thenReturn("manager");
+        when(request.getParameter("id")).thenReturn("1");
+        when(daoMock.sendOrder(any(Integer.class))).thenThrow(new RuntimeException("Errore DB"));
+        servlet.doGet(request, response);
+        verify(request).setAttribute(eq("exception"), any(RuntimeException.class));
+        verify(dispatcher).forward(request, response);
+    }
+
+    @Test
+    public void TC_2_4_6Success() throws ServletException, IOException {
+        Utente utente = mock(Utente.class);
+        when(session.getAttribute("utente")).thenReturn(utente);
+        when(utente.getRuolo()).thenReturn("manager");
+        when(request.getParameter("id")).thenReturn("1");
+        when(daoMock.sendOrder(any(Integer.class))).thenReturn(true);
+        servlet.doGet(request, response);
+        verify(response).addCookie(argThat(cookie -> cookie.getName().equals("notification")));
+        verify(response).sendRedirect(eq("null\\manage-orders"));
+    }
+
+}
